73d87a8e9db216897da8dbad9264248aa61187ba
Rafael Goto <rafael916goto@hotmail.com>
2018-04-02 15:29:26 -0300

    Prototipos das funções

--- lib.h
+++ lib.h
@@ -1,9 +1,12 @@
-#ifndef LIB_H 
+#ifndef LIB_H
 #define LIB_H
 
 int verificacao (int argc);
 double eulerFunctionDouble (unsigned int n);
 float eulerFunctionFloat (unsigned int n);
 long double eulerFunctionLDouble (unsigned int n);
+void chartFloat (unsigned int n);
+void chartDouble (unsigned int n);
+void chartLDouble (unsigned int n);
 
 #endif
