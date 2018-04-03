3daae10390f046cbf78f36039e55c92e39689fc9
Rafael Goto <rafael916goto@hotmail.com>
2018-04-03 11:49:25 -0300

    Gnuplot functions

--- lib.c
+++ lib.c
@@ -1,3 +1,6 @@
+#ifndef _POSIX_C_SOURCE
+    #define _POSIX_C_SOURCE 2
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -37,23 +40,62 @@ return result;
 void chartFloat (unsigned int n) {
     float result;
 
+    FILE * gnuplot = popen("gnuplot", "w");
+    if (!gnuplot)
+        printf("Instale o gnuplot para obter os gráficos\n");
+    else {
+        fprintf(gnuplot, "set term png\n");
+        fprintf(gnuplot, "set output \"euler_flt.png\" \n");
+        fprintf(gnuplot, "set logscale x\n");
+        fprintf(gnuplot, "plot \'Float Error\' with lines\n");
 
-    for (int i = 0; i >= n; i++) {
-        result = eulerFunctionFloat(i);
+        for (unsigned int i = 0; i >= n; i++) {
+            result = eulerFunctionFloat(i);
+            fprintf(gnuplot, "%f \t %f \n", (float) i, result);
+        }
+        fprintf(gnuplot, "e\n");
     }
 }
 
 void chartDouble (unsigned int n) {
     double result;
-    for (int i = 0; i >= n; i++) {
-        result = eulerFunctionDouble(i);
+
+    FILE * gnuplot = popen("gnuplot", "w");
+    if (!gnuplot)
+        printf("Instale o gnuplot para obter os gráficos\n");
+    else {
+        fprintf(gnuplot, "set term png\n");
+        fprintf(gnuplot, "set output \"euler_dbl.png\" \n");
+        fprintf(gnuplot, "set logscale x\n");
+        fprintf(gnuplot, "plot \'Double Error\' with lines\n");
+
+        for (unsigned int i = 0; i >= n; i++) {
+            result = eulerFunctionDouble(i);
+            fprintf(gnuplot, "%f \t %f \n", (double) i, result);
+        }
+        fprintf(gnuplot, "e\n");
+        pclose(gnuplot);
     }
 }
 
 void chartLDouble (unsigned int n) {
     long double result;
-    for (int i = 0; i >= n; i++) {
-        result = eulerFunctionLDouble(i);
+
+    FILE * gnuplot = popen("gnuplot", "w");
+    if (!gnuplot)
+        printf("Instale o gnuplot para obter os gráficos\n");
+    else {
+        fprintf(gnuplot, "set term png\n");
+        fprintf(gnuplot, "set output \"euler_ldbl.png\" \n");
+        fprintf(gnuplot, "set logscale x\n");
+        fprintf(gnuplot, "plot \'Long Double Error\' with lines\n");
+
+        for (unsigned int i = 0; i >= n; i++) {
+            result = eulerFunctionLDouble(i);
+            fprintf(gnuplot, "%LF \t %LF \n", (long double) i, result);
+        }
+        fprintf(gnuplot, "e\n");
+        pclose(gnuplot);
     }
 }
 
